40e78189b3dcefb8bd39796847e2e25d34f0719e
Charles Holbrow <charlesholbrow@gmail.com>
2013-05-21 21:32:33 -0700

    Comment

--- sort.js
+++ sort.js
@@ -9,15 +9,19 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
 
+    // publish the Numbers collection, sorted descending
     Meteor.publish('numbers', function() {
       return Numbers.find({}, {sort:{n:-1}, limit:100});
     });
+
+    // Add example documents: {n:1}, {n:2} ... {n:6}
     if (Numbers.find().count() === 0) {
       for (var i = 0; i < 6; i++) {
         var max = Numbers.find({}, {sort:{n:-1}, limit:1}).fetch()[0] || {n:0};
         Numbers.insert({n:max.n + 1});
       }
     }
+
   });
 }
 
